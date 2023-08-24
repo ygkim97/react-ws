63f94b5d87f5f511d836484fa328a6874a588cdf
ygkim97 <ygkim97>
2023-08-24 16:51:24 +0900

    feat: tailwind counter 페이지에 적용

--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -14,39 +14,89 @@ import {
   reset
 } from "@/redux/features/counterSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { useState } from "react";
 
-export default function Home() {
+export default function Counter() {
   const count = useAppSelector((state) => state.counterReducer.value);
   const dispatch = useAppDispatch();
+  const [amount, setAmount] = useState(0);
 
   return (
-    <main style={{ maxWidth: 1200, marginInline: "auto", padding: 20 }}>
-      <div
-        style={{
-          marginBottom: "4rem",
-          textAlign: "center"
-        }}
-      >
-        <h4 style={{ marginBottom: 16 }}>{count}</h4>
-        <div
-          style={{
-            margin: "0px 200px",
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr",
-            columnGap: "10px"
-          }}
-        >
-          <button onClick={() => dispatch(increment())}>increment</button>
-          <button onClick={() => dispatch(decrement())}>decrement</button>
-          <button onClick={() => dispatch(incrementByAmount(2))}>
+    <div className="w-screen h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full mx-auto py-16">
+        <h1 className="text-5xl text-center font-bold mb-6">Counter</h1>
+
+        <div className="px-6 py-4 my-3 w-3/4 mx-auto items-center">
+          <div className="mb-4 flex justify-end">
+            <span className="text-2xl mr-2 pt-0.5">AMOUNT</span>
+            <div className="flex flex-row border h-10 w-24 rounded-lg border-gray-400 relative">
+              <button
+                className="font-semibold border-r bg-red-700 hover:bg-red-600 text-white border-gray-400 h-full w-20 flex rounded-l focus:outline-none cursor-pointer"
+                onClick={() => (amount > 0 ? setAmount(amount - 1) : undefined)}
+              >
+                <span className="m-auto">-</span>
+              </button>
+              <input
+                type="hidden"
+                className="md:p-2 p-1 text-xs md:text-base border-gray-400 focus:outline-none text-center"
+                readOnly
+                name="custom-input-number"
+              />
+              <div className="bg-white w-24 text-xs md:text-base flex items-center justify-center cursor-default">
+                <span>{amount}</span>
+              </div>
+
+              <button
+                className="font-semibold border-l  bg-blue-700 hover:bg-blue-600 text-white border-gray-400 h-full w-20 flex rounded-r focus:outline-none cursor-pointer"
+                onClick={() => setAmount(amount + 1)}
+              >
+                <span className="m-auto">+</span>
+              </button>
+            </div>
+          </div>
+          <div className="bg-white shadow rounded-md w-full text-center mx-auto">
+            <div className="text-2xl">{count}</div>
+          </div>
+        </div>
+
+        <div className="w-full text-center mx-auto">
+          <button
+            type="button"
+            className="primary-button"
+            onClick={() => dispatch(increment())}
+          >
+            increment
+          </button>
+          <button
+            type="button"
+            className="success-button"
+            onClick={() => dispatch(decrement())}
+          >
+            decrement
+          </button>
+          <button
+            type="button"
+            className="error-button"
+            onClick={() => dispatch(incrementByAmount(amount))}
+          >
             incrementByAmount
           </button>
-          <button onClick={() => dispatch(decrementByAmount(2))}>
+          <button
+            type="button"
+            className="warning-button"
+            onClick={() => dispatch(decrementByAmount(amount))}
+          >
             decrementByAmount
           </button>
-          <button onClick={() => dispatch(reset())}>reset</button>
+          <button
+            type="button"
+            className="info-button"
+            onClick={() => dispatch(reset())}
+          >
+            reset
+          </button>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Providers } from "@/redux/provider";
+import "@/app/globals.css";
 
 export default function RootLayout({
   children
