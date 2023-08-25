018cdea1013bc2b834be9568f64fb1af31b9da8b
ygkim97 <ygkim97>
2023-08-25 15:36:43 +0900

    chore: fontawesome 플러그인 설치 및 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Providers } from "@/redux/provider";
 import "@/app/globals.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 export default function RootLayout({
   children
